5fc4183d0cabc303d67d83632a385ab61b29346b
paulmatata <paulmatata66@gmail.com>
2026-07-06 12:56:44 +0300

    restored database connection

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,10 @@
 <?php
 ob_start();
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "makueni_digital_hub";
-$port = 3306;
-//$host = getenv('DB_HOST');//Localhost";
-//$user = getenv('DB_USER');//"root";
-//$pass = getenv('DB_PASS');//"";
-//$db = getenv('DB_NAME');
-//$port = getenv('DB_PORT') ?: 3306;
+$host = getenv('DB_HOST');//Localhost";
+$user = getenv('DB_USER');//"root";
+$pass = getenv('DB_PASS');//"";
+$db = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: 3306;
 
 $conn = mysqli_connect(
     $host,
