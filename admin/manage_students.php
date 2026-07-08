619fe49bc955c9731c0cbd7247e7ed0a0eafa314
paulmatata <paulmatata66@gmail.com>
2026-07-08 14:26:01 +0300

    Update manage_students.php

--- admin/manage_students.php
+++ admin/manage_students.php
@@ -5,7 +5,7 @@ include '../includes/admin_auth.php';
 include '../includes/db.php';
 
 include '../includes/admin_header.php';
-
+include '../includes/admin_sidebar.php';
 $centre_id = $_SESSION['centre_id'];
 
 $role = $_SESSION['admin_role'];
@@ -81,7 +81,7 @@ $result = mysqli_stmt_get_result($stmt);
 $row_number = $offset + 1;
 
 ?>
-
+<div class="admin-content">
 <div class="container py-4">
 
 <div class="card border-0 shadow-lg rounded-4 p-4">
@@ -326,5 +326,5 @@ Reset Password
 </div>
 
 </div>
-
+</div>
 <?php include '../includes/admin_footer.php'; ?>
